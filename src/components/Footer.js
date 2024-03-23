d92a645c2a4c0f0e1360289caee9864d81c2a25f
PeaceSeeking <alniemaa34@gmail.com>
2024-03-23 12:34:29 +0300

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import logo from '../images/littlelemon_logo.png'
+function Footer()
+{
+    return(
+        <footer>
+            <section>
+                <div className='company-info'>
+                    <img src={logo} alt="company-logo"/>
+                    <p>We are family owned Mediterraneran resturant, focused on traditional recipes with a modern twist</p>
+                </div>
+                <div>
+                    <h3>Important Links</h3>
+                    <ul>
+                        <li><a href='/'>Home</a></li>
+                        <li><a href='/'>About</a></li>
+                        <li><a href='/'>Services</a></li>
+                        <li><a href='/'>Menu</a></li>
+                        <li><a href='/'>Reservations</a></li>
+                        <li><a href='/'>Order Online</a></li>
+                        <li><a href='/'>Lon in</a></li>
+                    </ul>
+                </div>
+                <div>
+                    <h3>Contact</h3>
+                    <ul>
+                        <li>Address: <br/> 123 Towncity, USA</li>
+                        <li>Phone: <br/> ++ 0123 456 789</li>
+                        <li>Email: <br/> little@lemon.com</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h3>Social Media Links</h3>
+                    <ul>
+                        <li><a href='https://web.facebook.com/'>Facebook</a></li>
+                        <li><a href='https://www.instagram.com/'>Instagram</a></li>
+                        <li><a href='https://twitter.com/'>Services</a></li>
+                    </ul>
+                </div>
+            </section>
+        </footer>
+    );
+}
+
+export default Footer;
